3273845b9bd72fc12dd1705875a317eebb5d35ff
shubhod <shubhodeep.213@gmail.com>
2017-12-08 15:47:01 +0530

    Add files via upload

---
+++ postbox.js
@@ -0,0 +1,24 @@
+console.log("postbox.js working");
+$(window).scroll(function(){
+  $('.parentbox').children('div').each(function(){
+    if(isScrolledIntoView($(this))){
+      $(this).children('span').text('visible');
+      console.log($(this).attr('class')+"visible");
+    }
+    else{
+      $(this).children('span').text('invisible');
+      console.log($(this).attr('class')+"invisble");
+    }
+  });
+});
+
+function isScrolledIntoView(element){
+ var pageTop = $(window).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+        var elementTop = $(element).offset().top;
+        var elementBottom = elementTop + $(element).height();
+
+       
+            return ((pageTop < elementTop) && (pageBottom > elementBottom));
+       
+}
